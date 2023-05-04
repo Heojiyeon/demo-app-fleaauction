6260ab8cbe6cac49946b846874359955786a9594
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-05-04 18:16:02 +0900

    feat: SSE 이벤트 스트림 기능 구현

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import EventSource from "react-native-sse";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [auctions, setAuctions] = useState([]);
+
+  useEffect(() => {
+    const url = new URL("https://api.fleaauction.world/v2/sse/event");
+
+    const eventSource = new EventSource(url);
+
+    eventSource.addEventListener("open", event => {
+      console.log("Open SSE connection.");
+    });
+
+    eventSource.addEventListener("sse.auction_viewed", event => {
+      const auction = JSON.parse(event.data);
+      auction.id = event.lastEventId;
+
+      setAuctions(prevAuctions => [...prevAuctions, auction]);
+    });
+
+    eventSource.addEventListener("error", event => {
+      if (event.type === "error") {
+        console.error("Connection error:", event.message);
+      } else if (event.type === "exception") {
+        console.error("Error:", event.message, event.error);
+      }
+    });
+
+    eventSource.addEventListener("close", event => {
+      console.log("Close SSE connection.");
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <ScrollView
@@ -10,92 +43,24 @@ export default function App() {
         pagingEnabled
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.firstRow}>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.firstRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
+        {auctions.map(auction => (
+          <View id={auction.id} style={styles.firstRowItem}>
+            <Text style={styles.auctionId}>작품ID: {auction.auctionId}</Text>
+            <Text style={styles.viewCount}>조회수: {auction.viewCount}</Text>
+          </View>
+        ))}
       </ScrollView>
       <ScrollView
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.secondRow}>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
-        <View style={styles.secondRowItem}>
-          <Text style={styles.auctionId}>auctionId</Text>
-          <Text style={styles.viewCount}>viewCount</Text>
-        </View>
+        {auctions.map(auction => (
+          <View id={auction.id} style={styles.firstRowItem}>
+            <Text style={styles.auctionId}>작품ID: {auction.auctionId}</Text>
+            <Text style={styles.viewCount}>조회수: {auction.viewCount}</Text>
+          </View>
+        ))}
       </ScrollView>
     </View>
   );
